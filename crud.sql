f1eca29e807c7257cc0cb6f670ae4b2d73a2647a
Ardha <ardharafano@gmail.com>
2022-01-19 00:14:13 +0700

    edit .sql
    edit .sql

--- crud.sql
+++ crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2022 at 05:45 PM
+-- Generation Time: Jan 18, 2022 at 06:13 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -46,6 +46,34 @@ INSERT INTO `anggota` (`id_anggota`, `username`, `nama`, `alamat`, `email`, `no_
 (6, 'Ardha', 'Ardha', 'Jakarta', 'ardha@urtimate.com', '0217423111', '2022-01-18'),
 (7, 'rafano', 'RAFANO', 'Bekasi', 'ardha@urtimate.com', '0217423111', '2014-05-18'),
 (8, 'Nara', 'Nara', 'Bekasi', 'ardharafano@gmail.com', '0217423111', '2022-01-27');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_anggota`
+-- (See below for the actual view)
+--
+CREATE TABLE `view_anggota` (
+`id_anggota` int(11)
+,`username` varchar(50)
+,`nama` varchar(50)
+,`alamat` varchar(50)
+,`email` varchar(30)
+,`no_hp` char(13)
+,`tanggal_gabung` varchar(72)
+,`selisih_hari` int(7)
+,`selisih_bulan` bigint(21)
+,`selisih_tahun` bigint(21)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_anggota`
+--
+DROP TABLE IF EXISTS `view_anggota`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_anggota`  AS  select `anggota`.`id_anggota` AS `id_anggota`,`anggota`.`username` AS `username`,`anggota`.`nama` AS `nama`,`anggota`.`alamat` AS `alamat`,`anggota`.`email` AS `email`,`anggota`.`no_hp` AS `no_hp`,date_format(`anggota`.`tanggal_gabung`,'%d %M %Y') AS `tanggal_gabung`,to_days(curdate()) - to_days(`anggota`.`tanggal_gabung`) AS `selisih_hari`,timestampdiff(MONTH,`anggota`.`tanggal_gabung`,current_timestamp()) AS `selisih_bulan`,timestampdiff(YEAR,`anggota`.`tanggal_gabung`,current_timestamp()) AS `selisih_tahun` from `anggota` ;
 
 --
 -- Indexes for dumped tables
